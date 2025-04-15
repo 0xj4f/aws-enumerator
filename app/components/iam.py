2371ed3d35d506f431a9771342d97e30d3547e5d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2025-04-15 21:00:20 +0800

    Update iam.py

--- app/components/iam.py
+++ app/components/iam.py
@@ -1,4 +1,4 @@
-import boto3, json, os
+import json, os
 
 def enumerate(session, path):
     os.makedirs(path, exist_ok=True)
@@ -8,6 +8,21 @@ def enumerate(session, path):
     users = iam_client.list_users()['Users']
     with open(f"{path}/users.json", "w") as f:
         json.dump(users, f, default=str, indent=2)
+
+    # Groups
+    groups = iam_client.list_groups()['Groups']
+    with open(f"{path}/groups.json", "w") as f:
+        json.dump(groups, f, default=str, indent=2)
+
+    # Map users to their groups - needs to be tested to other accounts
+    user_group_map = {}
+    for user in users:
+        username = user["UserName"]
+        user_groups = iam_client.list_groups_for_user(UserName=username)["Groups"]
+        user_group_map[username] = user_groups
+
+    with open(f"{path}/user_group_memberships.json", "w") as f:
+        json.dump(user_group_map, f, default=str, indent=2)
 
     # Roles
     roles = iam_client.list_roles()['Roles']
@@ -21,7 +36,6 @@ def enumerate(session, path):
 
     # Attached Policies per Role
     role_attached_policies = {}
-
     for role in roles:
         role_name = role['RoleName']
         attached_policies = iam_client.list_attached_role_policies(RoleName=role_name)['AttachedPolicies']
